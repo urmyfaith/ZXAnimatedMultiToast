1ad99fe1764aa8ea8916f6faf864c87cf8537e34
zuoxue <zuoxue@qq.com>
2016-11-26 09:49:54 +0800

    Initial Commit

---
+++ ZXAnimatedMultiToast/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZXAnimatedMultiToast
+//
+//  Created by zx on 26/11/2016.
+//  Copyright © 2016 z2xy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
